dcce2b0a7a53f6cc24efbcdf05b7ca53ee1ab547
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-08 17:49:06 +0800

    Update prog02_equal_number.py

--- prog02_equal_number.py
+++ prog02_equal_number.py
@@ -1,7 +1,7 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
 
 if num1 == num2:
     print("Equal")
-elif num1 != num2:
-    print("Not Equal")
